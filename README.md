a228d24a00212b503c546bf384eefdcf969c0ebf
Yehison Cieza Luna <>
2025-06-13 00:49:21 -0500

    testcase: se hace las primeras pruebas

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # test-playwright
 Automatización con playwright e integración con git actions
+# comandos para crear el proyecto
+1. √ npm init playwright@latest
+2. √ Do you want to use TypeScript or JavaScript? · TypeScript
+3. √ Where to put your end-to-end tests? · tests
+4. √ Add a GitHub Actions workflow? (y/N) · true
+5. √ Install Playwright browsers (can be done manually via 'npx playwright install')? (Y/n) · true
+6. Para correr antes ejecuta esto npx playwright install  
+
+luego npx playwright test
---
+++ tests/acciones.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect} from '@playwright/test';
+test.skip ('Test de Acciones', async ({page}) => {
+    await page.goto('https://testertestarudo.com/sandbox-para-pruebas-automatizadas/');
+    await page.fill('//input[@id="name"]', 'Juan Perez');
+    await page.keyboard.press('Enter'); 
+    await page.keyboard.insertText('Juan Perez');
+    await page.mouse.wheel(0, 1000);
+    await page.fill('//input[@id="email"]', 'test@prueba.com');
+    await page.fill('//input[@id="ageSlider"]', '19');
+    await page.click('//*[@type="submit"]');
+    await expect(page.getByText('Form submitted!')).toBeVisible();
+    await page.pause();
+});
+
+test.skip ('Test de Copy', async ({page}) => {
+    await page.goto('https://testertestarudo.com/sandbox-para-pruebas-automatizadas/');
+    await expect(page.locator('//h1[contains(text(), "Sandbox para Pruebas Automatizadas")]')).toBeVisible();
+    await expect(page.locator('//*[contains(text(), "Importancia del Testing")]')).toBeVisible();
+    await expect(page.locator('//p[contains(text(), "El testing es una parte crucial del desarrollo de software,")]')).toBeVisible();
+    await expect(page.locator('//*[text()= "Form submitted!"]')).toBeHidden();
+
+    await page.pause();
+});
+
+test.only ('Test de Select', async ({page}) => {
+    await page.goto('https://testertestarudo.com/sandbox-para-pruebas-automatizadas/');
+    await page.locator('//*[@id="developerRoles"]').selectOption('Backend Developer');
+    await expect(page.locator('//*[text()="Form submitted!"]')).toBeHidden();
+    //await page.locator('//*[@id="developerRoles"]').click();
+    await page.pause();
+});
